01166888f9919a844f72d25b04904b3990101503
Tom <121559122+Vikable@users.noreply.github.com>
2024-12-13 03:20:47 +0000

    main collection js

---
+++ scripts.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const categoryTitles = document.querySelectorAll(
+    ".main-collection__category-title"
+  );
+  const categories = document.querySelectorAll(".main-collection__category");
+
+  categoryTitles.forEach((title) => {
+    title.addEventListener("click", function () {
+      const category = this.getAttribute("data-category");
+
+      categoryTitles.forEach((t) =>
+        t.classList.remove("main-collection__category-title--active")
+      );
+      this.classList.add("main-collection__category-title--active");
+
+      categories.forEach((c) => {
+        c.classList.remove("main-collection__category--active");
+        if (c.getAttribute("data-category") === category) {
+          c.classList.add("main-collection__category--active");
+        }
+      });
+    });
+  });
+
+  // Встановити першу категорію активною за замовчуванням
+  if (categoryTitles.length > 0) {
+    categoryTitles[0].click();
+  }
+});
